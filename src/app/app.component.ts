e4ac76570db8a1850c18b2f43e459ab09ffb22fd
German <German>
2018-07-05 14:36:10 -0300

    Maquetacion calendario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { setHours, setMinutes } from 'date-fns';
+import { CalendarEvent } from 'angular-calendar';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  view: string = 'day';
+
+  viewDate: Date = new Date();
+
+  events: CalendarEvent[] = [
+    {
+      title: 'No event end date',
+      start: setHours(setMinutes(new Date(), 0), 3),
+      //color: colors.blue
+    },
+    {
+      title: 'No event end date',
+      start: setHours(setMinutes(new Date(), 0), 5),
+      //color: colors.yellow
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarModule, CalendarEvent } from 'angular-calendar';
-import { setHours, setMinutes } from 'date-fns';
+import { CalendarModule } from 'angular-calendar';
+
 
 
 import { AppComponent } from './app.component';
@@ -18,21 +18,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
-  view: string = 'day';
-
-  viewDate: Date = new Date();
-
-  events: CalendarEvent[] = [
-    {
-      title: 'No event end date',
-      start: setHours(setMinutes(new Date(), 0), 3),
-      //color: colors.blue
-    },
-    {
-      title: 'No event end date',
-      start: setHours(setMinutes(new Date(), 0), 5),
-      //color: colors.yellow
-    }
-  ];
-}
+export class AppModule {}
