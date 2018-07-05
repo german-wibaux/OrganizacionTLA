e7ef234bf6f0c555fe341f2442809a163e4cfa0d
German <German>
2018-07-05 13:40:37 -0300

    Agregando calendario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, CalendarEvent } from 'angular-calendar';
+import { setHours, setMinutes } from 'date-fns';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +12,27 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, CalendarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  view: string = 'day';
+
+  viewDate: Date = new Date();
+
+  events: CalendarEvent[] = [
+    {
+      title: 'No event end date',
+      start: setHours(setMinutes(new Date(), 0), 3),
+      //color: colors.blue
+    },
+    {
+      title: 'No event end date',
+      start: setHours(setMinutes(new Date(), 0), 5),
+      //color: colors.yellow
+    }
+  ];
+}
