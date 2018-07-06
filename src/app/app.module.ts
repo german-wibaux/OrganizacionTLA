fa69cd2b1224dc31dcdbc5206c0fde83395960cb
Daniela77 <danielacolamai@gmail.com>
2018-07-06 16:48:04 -0300

    Vista de editor de eventos funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
+import 'flatpickr/dist/flatpickr.css';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule } from 'angular-calendar';
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-
-
+ import { FlatpickrModule } from 'angularx-flatpickr';
 
 import { AppComponent } from './app.component';
 
@@ -14,8 +16,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    CommonModule,
+    FormsModule,
     BrowserModule,
     BrowserAnimationsModule, CalendarModule.forRoot(),
+    FlatpickrModule.forRoot(),
     NgbModalModule.forRoot()
   ],
   providers: [],
